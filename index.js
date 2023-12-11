2a24d714250202e55a694e933a163a5032f0e910
Emre Tarakci <tarakci.software@gmail.com>
2023-12-11 21:12:20 +0300

    Fix string value

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ app.get("/users", async (req, res) => {
 
 app.get("/user/:userId", async (req, res) => {
   try {
-    const userId = req.params.userId;
+    const userId = parseInt(req.params.userId);
     results = await getUser(userId);
     res.status(200).json(results.rows);
   } catch (error) {
